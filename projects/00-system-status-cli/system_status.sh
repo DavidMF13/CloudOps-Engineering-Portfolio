31ee7e4aa12db401d0cc0d0e90f0e0c169d39159
DavidMF13 <david.martinez.fal@hotmail.com>
2025-11-03 14:53:23 -0600

    feat(bash): Add output to historic file

--- projects/00-system-status-cli/system_status.sh
+++ projects/00-system-status-cli/system_status.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+exec >> Historic_logs.txt
+echo -e "System Status Run: $(date)"
+
 echo "========== SYSTEM STATUS =========="
 
 echo -e "$(uptime | awk -F'( |,|:)+' '{d=h=m=0; if ($7=="min") m=$6; else {if ($7~/^day/) {d=$6;h=$8;m=$9} else {h=$6;m=$7}}} {print d+0,"days,",h+0,"hours,",m+0,"minutes."}')\n$(hostnamectl | grep -E 'hostname|Operating System |Architecture|Kernel')"
@@ -40,3 +43,5 @@ echo -e "DISK USAGE: $DISK_USE%"
 echo "========= SYSTEM LOG STATUS =========="
 
 sudo grep "error: " /var/log/messages | tail | awk -F'error: ' '{split($1,a," "); print a[1], $2}'
+
+
